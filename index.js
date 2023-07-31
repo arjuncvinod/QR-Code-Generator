c7aaf5a406e6e572e2d2cdf8ec94638c962889aa
arjuncvinod <arjuncvinod007@gmail.com>
2023-07-31 22:59:12 +0530

    qr-image generated

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ inquirer
     const url= answers.URL;
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
-    fs.writeFile(URL.txt,url,(err)=>{
+    fs.writeFile("URL.txt",url,(err)=>{
         if(err) throw err;
         console.log("file saved");
     })
