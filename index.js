7b25379b8c26b45f73189ce9ba0d33141d970514
arjuncvinod <arjuncvinod007@gmail.com>
2023-07-31 22:52:46 +0530

    index.js updated

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ inquirer
     const url= answers.URL;
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+    fs.writeFile(URL.txt,url,(err)=>{
+        if(err) throw err;
+        console.log("file saved");
+    })
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
     } else {
-      // Something else went wrong
     }
   });
